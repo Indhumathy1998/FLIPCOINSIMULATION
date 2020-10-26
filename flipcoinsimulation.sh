bfb8d9ec052cc2ca24e05141c15b348fd43dab98
Indhumathy1998 <indhumathymani12@gmail.com>
2020-10-27 02:00:10 +0530

    Updated branch name

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -27,14 +27,3 @@ done
 echo "Number of times Heads has won:" $headcount
 echo "Number of times Tails has won:" $tailcount
 
-if [ $headcount -gt $tailcount ]
-then
-	echo "head won"
-	echo "head won by count of:" $headcount
-elif [ $headcount -lt $tailcount ]
-then
-	echo "tail won"
-	echo "tail won by count of:" $tailcount
-else
-	echo "tie"
-fi
